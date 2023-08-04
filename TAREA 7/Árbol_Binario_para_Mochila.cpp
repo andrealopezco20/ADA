69e216f25e922630f498dc7026bba7d95227edcb
andrealopezco20 <104209000+andrealopezco20@users.noreply.github.com>
2023-08-03 19:50:49 -0500

    Add files via upload

---
+++ TAREA 7/Árbol_Binario_para_Mochila.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Item {
+	int weight;
+	int value;
+};
+
+int knapsackBinary(int capacity, const vector<Item>& items, int idx, int currentWeight, int currentValue) {
+	if (currentWeight > capacity || idx == items.size()) {
+		return 0;
+	}
+	
+	// Incluir el elemento actual
+	int withCurrent = items[idx].value + knapsackBinary(capacity, items, idx + 1, currentWeight + items[idx].weight, currentValue + items[idx].value);
+	
+	// No incluir el elemento actual
+	int withoutCurrent = knapsackBinary(capacity, items, idx + 1, currentWeight, currentValue);
+	
+	return max(withCurrent, withoutCurrent);
+}
+
+int main() {
+	int capacity = 10;
+	vector<Item> items = {{2, 5}, {3, 8}, {4, 9}, {5, 10}};
+	
+	int maxValue = knapsackBinary(capacity, items, 0, 0, 0);
+	cout << "Maximo valor usando Arbol Binario: " << maxValue << endl;
+	
+	return 0;
+}
+
---
+++ TAREA 7/Árbol_Combinatorio_para_Mochila.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Item {
+	int weight;
+	int value;
+};
+
+int knapsackCombinatorial(int capacity, const vector<Item>& items, int idx, int currentWeight, int currentValue) {
+	if (idx == items.size() || currentWeight >= capacity) {
+		return currentValue;
+	}
+	
+	// Incluir el elemento actual
+	int withCurrent = 0;
+	if (currentWeight + items[idx].weight <= capacity) {
+		withCurrent = knapsackCombinatorial(capacity, items, idx + 1, currentWeight + items[idx].weight, currentValue + items[idx].value);
+	}
+	
+	// No incluir el elemento actual
+	int withoutCurrent = knapsackCombinatorial(capacity, items, idx + 1, currentWeight, currentValue);
+	
+	return max(withCurrent, withoutCurrent);
+}
+
+int main() {
+	int capacity = 10;
+	vector<Item> items = {{2, 5}, {3, 8}, {4, 9}, {5, 10}};
+	
+	int maxValue = knapsackCombinatorial(capacity, items, 0, 0, 0);
+	cout << "Maximo valor usando Arbol Combinarotio: " << maxValue << endl;
+	
+	return 0;
+}
+
+
